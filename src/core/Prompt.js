7ff6f67c5983762be9629af799ef0f8e9f21a071
hhertout <hugues.hertout@gmail.com>
2023-10-28 19:48:43 +0200

    feat: validate prompt

--- src/core/Prompt.js
+++ src/core/Prompt.js
@@ -2,25 +2,45 @@ import inquirer from "inquirer";
 import Tech from "../enums/tech.enum.js";
 
 class Prompter {
-  constructor() {}
-
-  prompt = async () => {
-    const answers = await inquirer.prompt([
-      {
-        type: 'list',
-        name: 'technology',
-        message: 'What technology do you want to use?',
-        choices: [Tech.VANILLA, Tech.REACT],
-      },
-      {
-        type: 'input',
-        name: 'snpx ubmoduleName',
-        message: 'What is the name of your submodule?',
-      },
-    ]);
-    console.log(answers);
-    return {};
-  };
+    /**
+     * Can throw error if validation fails
+     *
+     * @returns {Promise<{technology: string, submoduleName: string}>}
+     */
+    static prompt = async () => {
+        const result = await inquirer.prompt([
+            {
+                type: 'list',
+                name: 'technology',
+                message: 'What technology do you want to use?',
+                choices: [Tech.VANILLA, Tech.REACT],
+            },
+            {
+                type: 'input',
+                name: 'submoduleName',
+                message: 'What is the name of your submodule?',
+            },
+        ]);
+        this.#validatePromptValues(result);
+    };
+    /**
+     * @private
+     *
+     * Validate prompt value
+     *
+     * @param {{technology: any, submoduleName: string}} result
+     */
+    static #validatePromptValues = (result) => {
+        if (!result.submoduleName) {
+            throw new Error('Submodule name is required');
+        }
+        if (!result.technology) {
+            throw new Error('Technology is required');
+        }
+        if (!Object.values(Tech).includes(result.technology)) {
+            throw new Error('Invalid technology');
+        }
+    };
 }
 
 export default Prompter;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import { createSpinner } from 'nanospinner';
 import Prompter from './core/Prompt.js';
 
 const main = () => {
-  const spinner = createSpinner('Loading...').start();
-  sleep(2000).then((_) => {
+  const spinner = createSpinner('Starting...').start();
+  sleep(1000).then((_) => {
     spinner.stop();
-    new Prompter().prompt().then((_) => {
+    Prompter.prompt().then((answers) => {
       spinner.success({ text: 'Done!' });
+      return answers;
+    }).catch(err => {
+        spinner.error({ text: err.message });
     });
   });
 };

d7b64872500937282f54ac887aef07176a7fe0e6
hhertout <hugues.hertout@gmail.com>
2023-11-04 16:47:18 +0100

    test: adding more test to submodule builder

--- tests/integration/submoduleBuilder.test.js
+++ tests/integration/submoduleBuilder.test.js
@@ -1,18 +1,51 @@
 import SubmoduleBuilder from '../../src/core/SubmoduleBuilder.js';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 import DESTINATION_PATH_TEST from '../data/destinationPath.js';
 import { readdir } from 'fs/promises';
 
 describe('SubModuleBuilder integration tests', () => {
-  const url = fileURLToPath(import.meta.url);
-  const baseDir = dirname(url);
   test('SubModuleBuilder is alive', () => {
     expect(SubmoduleBuilder).toBeTruthy();
   });
   test('SubModuleBuilder is open', async () => {
-    const subModuleName = 'TestModule';
-    const subModuleBuilder = new SubmoduleBuilder(subModuleName, DESTINATION_PATH_TEST);
+    let subModuleName = 'TestModule';
+    const subModuleBuilder = new SubmoduleBuilder(
+      subModuleName,
+      DESTINATION_PATH_TEST
+    );
+    await subModuleBuilder.run();
+
+    for (const [key, value] of Object.entries(DESTINATION_PATH_TEST)) {
+      const dirs = await readdir(value);
+      const expectedFile = key.replaceAll('_', '.');
+      expect(dirs).toContain(expectedFile);
+    }
+  });
+});
+
+describe('Submodule builder with different submodule name', () => {
+  /**
+   * Test with different submodule name
+   */
+  test('test with different submodule name', async () => {
+    const subModuleName = 'testModule';
+    const subModuleBuilder = new SubmoduleBuilder(
+      subModuleName,
+      DESTINATION_PATH_TEST
+    );
+    await subModuleBuilder.run();
+
+    for (const [key, value] of Object.entries(DESTINATION_PATH_TEST)) {
+      const dirs = await readdir(value);
+      const expectedFile = key.replaceAll('_', '.');
+      expect(dirs).toContain(expectedFile);
+    }
+  });
+  test('test with different submodule name', async () => {
+    const subModuleName = 'testmodule';
+    const subModuleBuilder = new SubmoduleBuilder(
+      subModuleName,
+      DESTINATION_PATH_TEST
+    );
     await subModuleBuilder.run();
 
     for (const [key, value] of Object.entries(DESTINATION_PATH_TEST)) {

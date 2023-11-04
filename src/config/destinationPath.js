7278ecce7e329273e10bdbb23f592f2f34255eea
hhertout <hugues.hertout@gmail.com>
2023-11-04 19:58:40 +0100

    feat: adding solid templates

--- src/config/destinationPath.js
+++ src/config/destinationPath.js
@@ -3,11 +3,12 @@
  * @description Object config for destination path
  */
 const DESTINATION_PATH = {
-  controller_php: 'app/Http/Controllers',
-  react_app_jsx: 'assets/js/submodules',
   vanilla_app_module_js: 'assets/js/submodules',
+  react_app_jsx: 'assets/js/submodules',
   react_app_module_js: 'assets/js/submodules',
-  module_css: 'assets/css/submodules',
+  solid_app_jsx: 'assets/js/submodules',
+  solid_index_js: 'assets/js/submodules',
+  solid_view_blade_php: 'ressources/views/submodules',
 };
 
 Object.freeze(DESTINATION_PATH);
